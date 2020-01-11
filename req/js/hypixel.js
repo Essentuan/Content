2306e44a3b3ce4b40b3c131dbd1fe5235330d024
Essentuan <essentuan@gmail.com>
2020-01-11 04:15:47 +0000

    Members Page v0.1

--- req/js/hypixel.js
+++ req/js/hypixel.js
@@ -1,5 +1,5 @@
 var key = {
-  "personal": "a1951abd-50a3-469b-95bf-758e9ace498f",
+  "personal": "1c1ffc4c-e7b0-4dc2-a67b-4f3bd2690e7a",
   "guild": "5d59774b77ce84663abe90f6"
 }
 function getPlayer(user, vari) {
@@ -20,33 +20,24 @@ function getGuild(vari) {
       return(data.responseJSON.guild)
   }
 }
-function getHighestMode() {
-  let modes = getGuild("guildExpByGameType")
-  let xp = []
-  for (let i = 0; i < Object.keys(modes).length; i++) {
-    xp.push(modes[Object.keys(modes)[i]])
-  }
-  xp.sort(function(a, b){return b - a})
-  for (let n = 0; n < xp.length; n++) {
-    for (let i = 0; i < xp.length; i++) {
-      if (xp[n] == modes[Object.keys(modes)[i]]) {
-        console.log(Object.keys(modes)[i] + " | " + xp[n])
-        break;
-      }
-    }
-  }
-}
+//function getHighestMode() {
+  //let modes = getGuild("guildExpByGameType")
+  //let xp = []
+  //for (let i = 0; i < Object.keys(modes).length; i++) {
+    //xp.push(modes[Object.keys(modes)[i]])
+  //}
+  //xp.sort(function(a, b){return b - a})
+  //for (let n = 0; n < xp.length; n++) {
+    //for (let i = 0; i < xp.length; i++) {
+      //if (xp[n] == modes[Object.keys(modes)[i]]) {
+        //console.log(Object.keys(modes)[i] + " | " + xp[n])
+        //break;
+      //}
+    //}
+  //}
+//}
 function getNameFromUUID(uuid) {
   let url = "https://api.hypixel.net/player?key=" + key.personal + "&uuid=" + uuid
   let data = $.ajax({url: url, type: 'get', async: false, dataType: 'json',})
   return(data.responseJSON.player.displayname)
-}
-function getAllMembers() {
-  let members = getGuild("members")
-  let users = []
-  for (let i = 0; i < members.length; i++) {
-    console.log(i)
-    users.push(getNameFromUUID(members[i].uuid))
-  }
-  return(users)
 }
---
+++ req/js/members.js
@@ -0,0 +1,60 @@
+function getAllMembers() {
+  let members = getGuild("members")
+  let users = []
+  for (let i = 0; i < members.length; i++) {
+    users.push(getNameFromUUID(members[i].uuid))
+  }
+  return(users)
+}
+function checkGuild() {
+    let guildNew = getGuild()
+    let guildOld = JSON.parse(localStorage.getItem("guild"))
+    let equal 
+    if (guildNew.members.length != guildOld.members.length) {
+        equal = "force"
+        return(equal)
+    } else if (guildNew.ranks.length != guildOld.ranks.length) {
+        equal = "force"
+        return(equal)
+    } else {
+        for (let i = 0; i < guildNew.ranks.length; i++) {
+            if (guildNew.ranks[i].name != guildOld.ranks[i].name) {
+                equal = "force"
+                return(equal)
+            }
+        }
+        for (let i = 0; i < guildNew.members.length; i++) {
+            if (guildNew.members[i].uuid != guildOld.members[i].uuid) {
+                equal = "force"
+                return(equal)
+            }
+        }
+    }
+    return(equal)
+}
+function updateGuild(opt) {
+    if (opt != "force") {
+        if (localStorage.getItem("guild") == null) {
+            localStorage.setItem("guild", JSON.stringify(getGuild()))
+        }
+        if (localStorage.getItem("guildUS") == null) {
+            localStorage.setItem("guildUS", getAllMembers())
+        }
+    } else {
+        console.warn("Forcing reload of Guild Cache.")
+        localStorage.setItem("guild", JSON.stringify(getGuild()))
+        localStorage.setItem("guildUS", getAllMembers())
+    }
+}
+//<div class="item-hints"><div class="hint" data-position="4"><!-- is-hint --><img src="https://crafatar.com/avatars/8065a5b5d5a04ee29ff2d31c4d92c6f8?overlay" width="96px" height="96px"><span class="hint-radius"></span><div class="hint-content do--split-children"><p></p></div></div></div>   
+updateGuild(checkGuild())
+let rankList = JSON.parse(localStorage.getItem("guild")).ranks.sort(function(a, b) {return(b.priority - a.priority)})
+let membersList = JSON.parse(localStorage.getItem("guild")).members
+let namesList = localStorage.getItem("guildUS").split(",")
+$("#main").append('<h1>Guild Master</h1><hr><row id="GuildMaster"></row>')
+for (let i = 0; i < rankList.length; i++) {
+    $("#main").append('<h1>' + rankList[i].name + '</h1><hr><row id="'+ rankList[i].name.replace(/\s/g, '') + '"></row>')
+}
+for (let i = 0; i < membersList.length; i++) {
+    $("#" + membersList[i].rank.replace(/\s/g, '')).append('<div class="item-hints"><div class="hint" data-position="4"><!-- is-hint --><img src="https://crafatar.com/avatars/' + membersList[i].uuid + '?overlay" width="96px" height="96px"><span class="hint-radius"></span><div class="hint-content do--split-children"><p>' + namesList[i] + '</p></div></div></div>')
+}
